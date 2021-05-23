fcca31c06de28f80954e700df381eaf40027cbd4
swift1719 <ayushpawar1501@gmail.com>
2021-05-23 21:16:35 +0530

    Updates

---
+++ precache-manifest.03d645c429f74fa74c44ae99f8457147.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27830b32352e326224c5ff981e28dee5",
+    "url": "/Youtube_Spotify_Widget/index.html"
+  },
+  {
+    "revision": "4f347706c026ecc181e1",
+    "url": "/Youtube_Spotify_Widget/static/css/main.836eb79a.chunk.css"
+  },
+  {
+    "revision": "8e66bfe9f06a05629a8b",
+    "url": "/Youtube_Spotify_Widget/static/js/2.ac6d6df5.chunk.js"
+  },
+  {
+    "revision": "3205f2f97be94286aaab611b41fbc37d",
+    "url": "/Youtube_Spotify_Widget/static/js/2.ac6d6df5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f347706c026ecc181e1",
+    "url": "/Youtube_Spotify_Widget/static/js/main.8827cc19.chunk.js"
+  },
+  {
+    "revision": "f09ebd032a6574b1a278",
+    "url": "/Youtube_Spotify_Widget/static/js/runtime-main.4cbe99e2.js"
+  }
+]);
